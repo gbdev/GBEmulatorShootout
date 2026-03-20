d227cebc7036f77e80b4e07832e00acbd0fbc9f3
vulcandth <vulcandth@gmail.com>
2026-03-20 11:42:01 -0700

    Fix KiGB startup in CI

--- emulators/kigb.py
+++ emulators/kigb.py
@@ -9,10 +9,31 @@ class KiGB(Emulator):
     def __init__(self):
         super().__init__("KiGB", "http://kigb.emuunlim.com/", startup_time=1.6)
 
+    def _running_in_ci(self):
+        return os.environ.get("CI") or os.environ.get("GITHUB_ACTIONS")
+
+    def _write_allegro_config(self):
+        if not self._running_in_ci():
+            return
+
+        open("emu/kigb/allegro.cfg", "wt").write("""
+[graphics]
+gfx_card = GDIB
+gfx_cardw = GDIB
+
+[sound]
+digi_card = 0
+midi_card = 0
+
+[joystick]
+joytype = 0
+""".lstrip())
+
     def setup(self):
         download("http://kigb.emuunlim.com/kigb_win.zip", "downloads/kigb.zip")
         extract("downloads/kigb.zip", "emu/kigb")
         setDPIScaling("emu/kigb/kigb.exe")
+        self._write_allegro_config()
     
     def startProcess(self, rom, *, model, required_features):
         model = {DMG: 0, CGB: 1, SGB: 4}.get(model)
