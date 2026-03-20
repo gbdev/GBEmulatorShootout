9d19e4b039eb180ac98bfada90ebaf51611f3ed3
vulcandth <vulcandth@gmail.com>
2026-03-20 15:41:01 -0700

    Fix CI for VBA-M and VBA (#63)
    * Fix VBA and VBA-M Windows handling
    
    * Fix VBA SDL configuration in CI
    
    * Stabilize VBA SDL audio in CI
    
    * Apply DEP workaround to VBA in CI

--- emulators/vba.py
+++ emulators/vba.py
@@ -2,23 +2,58 @@ from util import *
 from emulator import Emulator
 from test import *
 import shutil
+import PIL.Image
+
+
+def _is_vbam_windows_x86_64_asset(name):
+    name = name.lower()
+    return (
+        name.endswith(".zip")
+        and "win" in name
+        and "x86_64" in name
+        and "arm64" not in name
+        and "debug" not in name
+        and "symbols" not in name
+        and "pdb" not in name
+    )
 
 
 class VBA(Emulator):
     def __init__(self):
         super().__init__("VisualBoyAdvance", "https://sourceforge.net/projects/vba", startup_time=0.6)
 
+    def _running_in_ci(self):
+        return os.environ.get("CI") or os.environ.get("GITHUB_ACTIONS")
+
     def setup(self):
         download("https://sourceforge.net/projects/vba/files/latest/download", "downloads/vba.zip")
         extract("downloads/vba.zip", "emu/vba")
-        setDPIScaling("emu/vba/VisualBoyAdvance.exe")
-        shutil.copyfile(os.path.join(os.path.dirname(__file__), "vba.ini"), "emu/vba/vba.ini")
+        if self._running_in_ci():
+            setAppCompatLayers("emu/vba/VisualBoyAdvance-SDL.exe", "DisableNXShowUI", "HIGHDPIAWARE")
+        else:
+            setDPIScaling("emu/vba/VisualBoyAdvance-SDL.exe")
+        shutil.copyfile(os.path.join(os.path.dirname(__file__), "VisualBoyAdvance.cfg"), "emu/vba/VisualBoyAdvance.cfg")
         download("https://gbdev.gg8.se/files/roms/bootroms/sgb_boot.bin", "emu/vba/sgb_boot.bin")
         download("https://gbdev.gg8.se/files/roms/bootroms/cgb_boot.bin", "emu/vba/cgb_boot.bin")
         download("https://gbdev.gg8.se/files/roms/bootroms/dmg_boot.bin", "emu/vba/dmg_boot.bin")
 
     def startProcess(self, rom, *, model, required_features):
-        return subprocess.Popen(["emu/vba/VisualBoyAdvance-SDL.exe", os.path.abspath(rom)], cwd="emu/vba")
+        env = os.environ.copy()
+        env["SDL_AUDIODRIVER"] = "dummy"
+        return subprocess.Popen(["emu/vba/VisualBoyAdvance-SDL.exe", os.path.abspath(rom)], cwd="emu/vba", env=env)
+
+    def getScreenshot(self):
+        screenshot = getScreenshot(self.title_check)
+        if screenshot is None:
+            return None
+
+        if screenshot.size == (320, 288):
+            background = screenshot.getpixel((screenshot.size[0] - 1, screenshot.size[1] - 1))
+            normalized = PIL.Image.new(screenshot.mode, screenshot.size, background)
+            normalized.paste(screenshot.crop((0, 2, screenshot.size[0], screenshot.size[1])), (0, 0))
+            screenshot = normalized.resize((160, 144), resample=PIL.Image.Resampling.NEAREST)
+
+        return screenshot
 
 
 class VBAM(Emulator):
@@ -27,7 +62,12 @@ class VBAM(Emulator):
         self.title_check = lambda title: "VisualBoyAdvance-M" in title
 
     def setup(self):
-        downloadGithubRelease("visualboyadvance-m/visualboyadvance-m", "downloads/vba-m.zip", filter=lambda n: "Win" in n and "64" in n and n.endswith(".zip"))
+        downloadGithubRelease(
+            "visualboyadvance-m/visualboyadvance-m",
+            "downloads/vba-m.zip",
+            filter=_is_vbam_windows_x86_64_asset,
+            require_asset=True,
+        )
         extract("downloads/vba-m.zip", "emu/vba-m")
         setDPIScaling("emu/vba-m/visualboyadvance-m.exe")
         download("https://gbdev.gg8.se/files/roms/bootroms/dmg_boot.bin", "emu/vba-m/dmg_boot.bin")
