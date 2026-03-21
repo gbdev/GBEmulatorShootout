e49c24ade5b7abb2a92038fa5a07aee1aed6d522
Docheinstein <dottorestefano97@gmail.com>
2026-03-21 23:39:53 +0100

    Add DocBoy in CGB version too

--- emulators/docboy.py
+++ emulators/docboy.py
@@ -10,14 +10,15 @@ class DocBoy(Emulator):
         super().__init__("DocBoy", "https://github.com/Docheinstein/docboy", startup_time=1, features=(PCM,))
 
     def setup(self):
-        downloadGithubRelease("Docheinstein/docboy", "downloads/docboy.zip", filter=lambda n: "dmg" in n and "win" in n)
-        extract("downloads/docboy.zip", "emu/docboy")
-        setDPIScaling("emu/docboy/docboy-sdl.exe")
-        shutil.copyfile(os.path.join(os.path.dirname(__file__), "docboy.ini"),
-                        "emu/docboy/docboy.ini")
+        for model in ["dmg", "cgb"]:
+            downloadGithubRelease(f"Docheinstein/docboy", f"downloads/docboy-{model}.zip", filter=lambda n: model in n and "win" in n)
+            extract(f"downloads/docboy-{model}.zip", f"emu/docboy-{model}")
+            setDPIScaling(f"emu/docboy-{model}/docboy-sdl.exe")
+            shutil.copyfile(os.path.join(os.path.dirname(__file__), "docboy.ini"), f"emu/docboy-{model}/docboy.ini")
 
     def startProcess(self, rom, *, model, required_features):
-        if model != DMG:
+        model = {DMG: "dmg", CGB: "cgb"}.get(model)
+        if model is None:
             return None
-        return subprocess.Popen(["emu/docboy/docboy-sdl.exe", os.path.abspath(rom), "-c", "docboy.ini", "-z", "1"],
-                                cwd="emu/docboy")
+        return subprocess.Popen([f"emu/docboy-{model}/docboy-sdl.exe", os.path.abspath(rom), "-c", "docboy.ini", "-z", "1"],
+                                cwd=f"emu/docboy-{model}")
